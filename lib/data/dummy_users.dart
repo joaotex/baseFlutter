695e6e34b0abfef929df2123b08ee634cd611948
Joao Marcos Teixeira <joao.tex97@gmail.com>
2020-05-17 22:48:24 -0300

    first commit

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_crud/models/User.dart';
+
+const DUMMY_USERS = {
+  '1' : const User(
+    id: '1',
+    name: 'John',
+    email: 'john@john.com',
+    avatarUrl: 'https://img.icons8.com/office/16/000000/person-male.png',
+  )
+};
